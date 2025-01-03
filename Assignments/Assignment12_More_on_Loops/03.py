c8a1535a4f8ceeca7dbe1c3151b6d442cdcd1e1b
amankumar62030 <amankumarakk536@gmail.com>
2025-01-04 00:27:55 +0530

    changes

---
+++ Assignments/Assignment12_More_on_Loops/03.py
@@ -0,0 +1,11 @@
+# Write a python script to print all Prime numbers under 100
+
+for i in range(2,101):
+    is_prime = True
+
+    for j in range(2,i):
+        if i%j==0:
+            is_prime = False
+            break
+    if is_prime:
+        print(i,end=' ')
